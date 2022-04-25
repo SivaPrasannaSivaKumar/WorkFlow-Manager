59c6442c488cd4e070783a478446f151aa7705aa
SivaPrasannaSivaKumar <sivaprasanna.s@heptagon.in>
2022-04-25 15:29:40 +0530

    Added few details

--- src/app/module/auth/sign-in/sign-in.component.ts
+++ src/app/module/auth/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private route: Router, private http: HttpClient) { }
+
+  public signInForm !: FormGroup;
 
   ngOnInit(): void {
+    this.signInForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
+  login() {
+    this.http.get<any>("http://localhost:3000/signUpUsers")
+      .subscribe(res => {
+        const user = res.find((a: any) => {
+          return a.email === this.signInForm.value.email && a.password === this.signInForm.value.password
+        });
+        if (user) {
+          this.signInForm.reset();
+          this.route.navigate([''])
+        } else {
+          alert("User not found");
+        }
+      }, err => {
+        alert("Something went wrong");
+      });
+  }
 }
--- src/app/module/auth/sign-up/sign-up.component.ts
+++ src/app/module/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  public UserRegister !: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private route: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.UserRegister = this.formBuilder.group({
+      userName:['',Validators.required],
+      userEmail:['',Validators.required],
+      userPassword:['',Validators.required]
+    })
+  }
+
+  saveUser(){
+    this.http.post<any>("http://localhost:3000/User",this.UserRegister.value).subscribe(res => {
+      this.UserRegister.reset();
+    alert("User Registered Successfully!");
+
+    },
+    err =>{
+      console.log("Something went wrong");
+    });
   }
 
 }
