0e0485a8b0444735c5f4679d62239cf21cac4187
SivaPrasannaSivaKumar <sivaprasanna.s@heptagon.in>
2022-04-30 20:03:51 +0530

    Added few details

--- src/app/module/sign-up/sign-up.component.ts
+++ src/app/module/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { RestService } from 'src/app/rest.service';
